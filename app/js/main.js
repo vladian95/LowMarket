26dadcdf2d3f26eaeb84ea32698cb8ac547b2ff3
Chesy <chesy47@gmail.com>
2021-01-11 16:48:03 +0200

    Card page --DONE

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function() {
     
+
+//Tabs(Card Page)    
+$('.wrapper .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+        $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+        $('.wrapper .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
 });
