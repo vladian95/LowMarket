b2871473b14c0880ebbd50d92c6e5a2d87125714
Chesy <chesy47@gmail.com>
2021-01-14 21:07:30 +0200

    Cabinet Page --DONE

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,15 @@ $('.wrapper .tab').on('click', function(event) {
         return false;
     });
 
+//Tabs(Cabinet Page)   
+$('.cabinet__wrapper .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+        $('.cabinet__wrapper').find('.tab-item').removeClass('active-tab').hide();
+        $('.cabinet__wrapper .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
 });
+
